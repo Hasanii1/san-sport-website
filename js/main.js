6a223e99ed11333a8563992206a8d0ebed1e0bce
nilshehu <projektdigital2@gmail.com>
2025-11-11 23:45:05 +0100

    san

--- js/main.js
+++ js/main.js
@@ -21,4 +21,53 @@ document.addEventListener('DOMContentLoaded', () => {
 			applyTheme(newTheme);
 		});
 	}
+
+
+	const clickable = document.querySelectorAll('mainoo.html');
+	clickable.forEach(img => {
+		img.addEventListener('click', (e) => {
+			const target = img.getAttribute('data-target');
+			if (target) {
+				window.location.href = target;
+			}
+		});
+		img.style.cursor = 'pointer';
+	});
 });
+
+let slideIndex = 1;
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let slides = document.getElementsByClassName('slide');
+    let dots = document.getElementsByClassName('dot');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+
+    slides[slideIndex - 1].classList.add('active');
+    dots[slideIndex - 1].classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+});
+
